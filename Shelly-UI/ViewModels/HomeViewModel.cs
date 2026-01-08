0a288325031af839757cf42fe531b351c0c78b1f
caroline <carolinemariesnyder@gmail.com>
2026-01-08 13:23:24 -0500

    Add InstalledPackages list to HomeViewModel and update HomeWindow layout

--- Shelly-UI/ViewModels/HomeViewModel.cs
+++ Shelly-UI/ViewModels/HomeViewModel.cs
@@ -1,16 +1,22 @@
 using System;
+using System.Collections.ObjectModel;
+using PackageManager.Alpm;
 using ReactiveUI;
 
 namespace Shelly_UI.ViewModels;
 
-public class HomeViewModel  : ViewModelBase, IRoutableViewModel
+public class HomeViewModel : ViewModelBase, IRoutableViewModel
 {
-    
+    public HomeViewModel(IScreen screen)
+    {
+        HostScreen = screen;
+        InstalledPackages = new ObservableCollection<AlpmPackage>(new AlpmManager().GetInstalledPackages());
+    }
+
     // Reference to IScreen that owns the routable view model.
     public IScreen HostScreen { get; }
 
-    public HomeViewModel(IScreen screen) => HostScreen = screen;
-    
     public string UrlPathSegment { get; } = Guid.NewGuid().ToString().Substring(0, 5);
-    
+
+    public ObservableCollection<AlpmPackage> InstalledPackages { get; set; }
 }
